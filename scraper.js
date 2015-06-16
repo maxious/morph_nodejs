cd28a9a1510de5b803e299524f5949771327cd1f
Alexander Sadleir <maxious@lambdacomplex.org>
2015-06-16 17:47:46 +1000

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -5,3 +5,16 @@
 // Sorry.
 
 console.log("Hello world!");
+var phantom = require('phantom');
+
+phantom.create(function (ph) {
+  ph.createPage(function (page) {
+    page.open("http://www.google.com", function (status) {
+      console.log("opened google? ", status);
+      page.evaluate(function () { return document.title; }, function (result) {
+        console.log('Page title is ' + result);
+        ph.exit();
+      });
+    });
+  });
+});
